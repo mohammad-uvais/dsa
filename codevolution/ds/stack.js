8a4abf6288983c2948764b3d5eb59318e7d1f82c
Uvais <mohammad.uvais@healthmug.com>
2025-01-03 18:35:47 +0530

    stack ds

---
+++ codevolution/ds/stack.js
@@ -0,0 +1,11 @@
+// Stack is a sequential collection of elements that follows the principal of LIFO
+// The last element inserted into the stack is the first element to be reomved
+// Stack is an abstract data type. It is defined by its behaviour rather than being a mathematical model
+// The stack data structure supports two main operations
+// -- push, which adds an element to the collection
+// -- pop, which removes the most recently added element from the collection
+
+// Stack usage
+// browser history tracking
+// undo operation when typing
+// Call stack in JS runtime
