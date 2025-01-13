63743f1778d9d49d17477e532b03e6dfb2e39846
Mohammad Uvais <uvaismansoori@gmail.com>
2025-01-14 01:35:34 +0530

    trees ds

---
+++ codevolution/ds/tree.js
@@ -0,0 +1,52 @@
+/**
+ * A tree is hierarchical data structure that consists of nodes connected by edges
+ * A tree is a non linear data structure, compared to arrays, linked lists, stacks and queues which are linear data structures
+ * In linear DSs, the time required to search is proportional to the size of the data set
+ * Trees however, owing to the nonlinear nature allow quicker and easier access to the data
+ * A tree will not contain any loops or cycles.
+ */
+
+/**
+ * Tree Usage
+ *
+ * File systems for directory structure
+ * A family tree
+ * An organisation tree
+ * DOM
+ * Chat bots
+ * Abstract syntax trees
+ */
+
+// Note -  Like linked lists, trees are something asked about during interviews
+
+/**
+ * Binary Tree
+ *
+ * A binary tree is a tree data structure in which each node has at most two children
+ * They are referred to as left child and right child
+ */
+
+/**
+ * Binary Search Tree
+ *
+ * The value of each left node must be smaller than the parent node
+ * The value of each right node must be greater than the parent node
+ * This is in addtion to each node has at most two children
+ */
+
+/**
+ * Binary Search Tree Operations
+ *
+ * Insertion - To add a node to the tree
+ * Search - To find a node given its value
+ * DFS & BFS - To visit all nodes in the tree
+ * Deletion - To remove a node given its value
+ */
+
+/**
+ * Binary Search Tree Usage
+ *
+ * Searching
+ * Sorting
+ * To implement abstract data types such as lookup tables and priority queues
+ */
