8287e13fed319ff3ca46e98f76f6c431b761ff85
Mohammad Uvais <uvaismansoori@gmail.com>
2025-01-16 22:16:35 +0530

    ending summary

---
+++ codevolution/ds/summary.js
@@ -0,0 +1,33 @@
+/**
+ * Built-in DS
+ *
+ * Arrays
+ * Objects
+ * Sets
+ * Maps
+ */
+
+/**
+ * Custom DS
+ * Stacks
+ * Queues
+ * Circular Queues
+ * Linked lists
+ * Linked lists with tail
+ * Doubly linked lists
+ * Hash Tables
+ * Trees
+ * Binary Search Trees
+ * Graph
+ */
+
+/**
+ * Further Learning
+ *
+ * Priority queue
+ * Circular linked lists
+ * AVL trees, red black trees and tries
+ * Directed acyclic graphs
+ * Graph Algorithms - Prim's, Kruskal's and Floyd's Algos
+ * Solve problems that utilize these DS
+ */
