2993d5c8b5753c0c9f841b279500283266658965
Mohammad Uvais <uvaismansoori@gmail.com>
2025-01-16 22:45:17 +0530

    dsa tut intro

---
+++ code-step-by-step/dsa-js/intro.js
@@ -0,0 +1,48 @@
+/**
+ * What is DS?
+ * A way to organize data so that we can use this data efficiently
+ *
+ * Types of DS
+ * Primitive and Non-primitive
+ *
+ * Primitive --> integer, float, character, pointer
+ * Non-Primitive --> Arrays, Lists, Files
+ *
+ * Lists --> Linear lists, Non-linear lists
+ *
+ * Linear Lists --> Stacks, Queues
+ * Non-linear lists --> Graphs, Trees
+ *
+ * DS to be covered in this series -
+ * Array
+ * Stack
+ * Queue
+ * Tree
+ * Graph
+ * Map
+ * Set
+ */
+
+/**
+ * What is Algo?
+ * Steps or process to arrange data
+ */
+
+/**
+ * Operations of DS
+ *
+ * Traversal --> Follow along the path
+ * Insertion
+ * Deletion
+ * Searching
+ * Sorting
+ * Merging
+ *
+ * Different algorithms are used for these operations
+ *
+ * Insertion
+ * Recursion
+ * Search
+ * Sorting
+ * Merge
+ */
